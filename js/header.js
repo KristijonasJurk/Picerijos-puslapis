559c27482c0c8b171331ecd6aca10b3cc616e824
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-02 00:28:44 +0200

    styling scroll header

--- js/header.js
+++ js/header.js
@@ -1,11 +1,14 @@
 const header = document.querySelector('header');
 
 function checkClass(header) {
-    const headerTop = document.querySelector('.headerTop');
-    const headerBottom = document.querySelector('.headerBottom');
+    // const headerTop = document.querySelector('.headerTop');
+    const headerBottomUl = document.querySelector('.headerBottom ul li');
+    const headerBottom = document.querySelector('.headerBottom ul');
     if (header.classList.contains('headerScroll')) {
-        // headerTop.style.transform = 'scale(0)';
-        // headerBottom.classList.add('fixedHeader');
+        headerBottom.classList.add('fixedHeader');
+        const image = document.createElement('img');
+        image.src = `../img/logo.jpg`;
+        headerBottom.insertBefore(image, headerBottomUl);
     }
 }
 checkClass(header);
