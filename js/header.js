4aecfc896aabbc025c16020bc46cf5e72bacf630
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-02 16:39:55 +0200

    finally figured out a smooth way to introduce img

--- js/header.js
+++ js/header.js
@@ -1,29 +1,31 @@
 const header = document.querySelector('header');
 
 function checkClass(header) {
-    // const headerTop = document.querySelector('.headerTop');
-    const headerBottomUl = document.querySelector('.headerBottom ul li');
     const headerBottom = document.querySelector('.headerBottom ul');
     if (header.classList.contains('headerScroll')) {
         headerBottom.classList.add('fixedHeader');
-        const image = document.createElement('img');
-        image.src = `../img/logo.jpg`;
-        headerBottom.insertBefore(image, headerBottomUl);
         setTimeout(launchImage, 300);
+    } else {
+        headerBottom.classList.remove('fixedHeader');
+        setTimeout(removeImage, 300);
     }
 }
 function launchImage() {
     const image = document.querySelector('.headerLinks img')
     image.style.width = '32px';
 }
+function removeImage() {
+    const image = document.querySelector('.headerLinks img')
+    image.style.width = '0px';
+}
 
 window.onscroll = function () {
     const windowHeight = window.pageYOffset;
-    if (windowHeight > 150) {
+    if (windowHeight > 50) {
         checkClass(header);
         document.querySelector("header").classList.add('headerScroll');
-        return;
-    } else {
+    } else if (windowHeight < 50) {
         document.querySelector("header").classList.remove('headerScroll');
+        checkClass(header);
     }
 }
