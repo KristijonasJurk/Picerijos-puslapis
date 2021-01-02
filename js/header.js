eb6c15f7f513c27f70ee5d95faf042fbfa4f799b
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-02 15:46:51 +0200

    added appearing image animation to header.js

--- js/header.js
+++ js/header.js
@@ -9,6 +9,11 @@ function checkClass(header) {
         const image = document.createElement('img');
         image.src = `../img/logo.jpg`;
         headerBottom.insertBefore(image, headerBottomUl);
+        setTimeout(launchImage, 300);
     }
 }
 checkClass(header);
+function launchImage() {
+    const image = document.querySelector('.headerLinks img')
+    image.style.width = '32px';
+} 
